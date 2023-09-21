aa9a894d1a7cb1f068cfde9ecb071f2a30850a70
fish-r <gyf8800@gmail.com>
2023-09-21 19:11:19 +0800

    feature: login api returns jwt

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 
 port = 8080
 const app = express()
@@ -11,11 +13,36 @@ app.get('/', (req, res) => {
     res.json({ msg: "welcome" })
 })
 
-// error 404
-app.use((req, res, next) => {
-    let err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+const users = [
+    { id: 1, username: 'abc', password: '123' },
+    { id: 2, username: 'T1234567B', password: 'password2' },
+];
+
+// returns a jwt for authentication
+app.post('/login', (req, res) => {
+    try {
+        const { username, password } = req.body;
+        console.log(req.body)
+        if (!username || !password) {
+            return res.status(400).json({ message: 'Invalid username or password' })
+        }
+        const user = users.find((u) => u.username === username && u.password === password);
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+        // secret can be found in .env
+        const token = jwt.sign({ id: user.id, username: user.username }, process.env.SECRET, {
+            expiresIn: '30m',
+        });
+        console.log(token)
+        res.status(200).json({ token });
+
+    } catch (error) {
+        res.status(500).send({
+            "error": error
+        })
+    }
+
 });
 
 // error logger
