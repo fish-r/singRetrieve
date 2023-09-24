af970873fabee7627f8bad63127190416dcc58a7
fish-r <gyf8800@gmail.com>
2023-09-24 16:20:39 +0800

    chore: change to prod base url

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "http://3.27.86.223:8080/",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
